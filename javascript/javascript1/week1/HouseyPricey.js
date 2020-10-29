2b81f09a099fa968a662214cde8e6abe0b924245
Enqira <moenkira@yahoo.com>
2020-10-29 17:37:59 +0100

    HouseyPricey App

---
+++ javascript/javascript1/week1/HouseyPricey.js
@@ -0,0 +1,13 @@
+
+function calculateIfhouseIsExpensive (name, wide, deep, hight, gardenSizeInM2, houseCost){
+    volumeInMeters = wide * deep * hight;
+    housePrice = (volumeInMeters * 2.5 * 1000) + (gardenSizeInM2 * 300);
+    if (housePrice > houseCost){
+        console.log(name + " bougth the house for " + houseCost + ". The calculated value of this house is: " + housePrice + ". It was a good purshase!");
+    }
+    else{
+        console.log(name + " bougth the house for " + houseCost + ". The calculated value of this house is: " + housePrice + ". It was a bad purshase!");
+    }
+}
+calculateIfhouseIsExpensive("Peter", 8, 10, 10, 100, 2500000);
+calculateIfhouseIsExpensive("Julia", 5, 11, 8, 70, 1000000);
