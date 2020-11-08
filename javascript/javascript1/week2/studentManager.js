fbea7b0a755509aa2bafb574509323ba1e34e6da
Enqira <moenkira@yahoo.com>
2020-11-08 19:20:22 +0100

    deleted unecesary code comented

--- javascript/javascript1/week2/studentManager.js
+++ javascript/javascript1/week2/studentManager.js
@@ -1,36 +1,5 @@
-// const class07Students = ["mo", "mmm"];
-// function addStudentToClass(studentName) {
-//     let countedNumberOfStudents = getNumberOfStudents()
-//     if (studentName == ""){
-//         return "empty array"
-//     } 
-    
-//     else if ( studentName == "Queen"){
-//         class07Students.push(studentName)
-//         return
-//     }
-//     else if ( class07Students.includes(studentName)){
-//         return "already included"
-//     }
-//     else if (countedNumberOfStudents > 5){
-//         return "array full"
-//     }
-    
-//     else{
-//         class07Students.push(studentName)
-//         return "student added"
-//     }
-// }
 
-// function getNumberOfStudents() {
-//   var nrOfStudents 
-//   nrOfStudents = class07Students.length
-//   console.log("The number of students is " + nrOfStudents  + ".")
-//   return nrOfStudents
-// }
-// console.log(addStudentToClass("Enqira")) 
-
-const class07Students = ["Mohammed", "Jens"];
+const class07Students = ["mo", "mmm"];
 function addStudentToClass(studentName) {
 let countedNumberOfStudents = getNumberOfStudents();
 if (studentName === "") {
