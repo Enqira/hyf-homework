d4fa2f452b5cdf4cf76f13c13ed2e02dd22db290
Enqira <moenkira@yahoo.com>
2020-11-18 23:53:59 +0100

    first part of voice assistant app

---
+++ javascript/javascript1/week4/voiceAssistant.js
@@ -0,0 +1,17 @@
+const todo = ["fishing", "singing"]
+const names = []
+function getReply(command) {
+    for (i=0; i<command.length; i++){   //we loop througth the command
+        if (command[i] == "name"){     //if the command contains the world name
+            nameFunc(command)          //we run nameFunc function
+        }
+    }
+}
+
+
+
+
+
+nameFunc(command){
+
+}
