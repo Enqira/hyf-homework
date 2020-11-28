5dbf5b510766bf40a2b9b95edde5dd3a0f2ecf08
Enqira <moenkira@yahoo.com>
2020-11-28 12:34:36 +0100

    finished hyfBay exercise

--- javascript/javascript2/week1/hyfBay/main.js
+++ javascript/javascript2/week1/hyfBay/main.js
@@ -5,15 +5,25 @@ console.log(products);
 
 function renderProducts(products) {
   for (let i = 0; i < products.length; i++) {
+    //   select and create tags
     const ulTag = document.querySelector("ul");
     const liTag = document.createElement("li");
     const nameTag = document.createElement("h3");
     const pricetag = document.createElement("span");
     const ratingTag = document.createElement("span");
+    const br = document.createElement("br");
 
-    liTag.innerHTML;
+    // populate tags
+    nameTag.innerHTML = products[i].name;
+    pricetag.innerHTML = "price: " + products[i].price;
+    ratingTag.innerHTML = "rating: " + products[i].rating;
+
+    // append tags
+    liTag.appendChild(nameTag);
+    liTag.appendChild(pricetag);
+    liTag.appendChild(br);
+    liTag.appendChild(ratingTag);
     ulTag.appendChild(liTag);
-    // console.log(products[i].name);
   }
 }
 renderProducts(products);
