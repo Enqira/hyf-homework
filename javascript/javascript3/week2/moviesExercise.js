4c498a10a2e6dfb07c6ae0fd794bfdcd587fe687
Enqira <moenkira@yahoo.com>
2021-01-08 04:39:54 +0100

    finished exercise movies

--- javascript/javascript3/week2/moviesExercise.js
+++ javascript/javascript3/week2/moviesExercise.js
@@ -1,30 +1,28 @@
 // Create an array of bad movies
+const moviesUrl =
+  "https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json"
 const fetchBadMovies = () => {
-  fetch(
-    "https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json"
-  )
+  fetch(moviesUrl)
     .then(res => res.json())
-    .then(res =>
+    .then(res => {
       res
         .filter(movie => movie.rating < 3)
         .map(badMovies => console.log(badMovies.title))
-    )
+    })
     .catch(err => console.log(err))
 }
 fetchBadMovies()
 
 // Creat an array of bad movies since year 2000
 const fetchBadMovies2000 = () => {
-  fetch(
-    "https://gist.githubusercontent.com/pankaj28843/08f397fcea7c760a99206bcb0ae8d0a4/raw/02d8bc9ec9a73e463b13c44df77a87255def5ab9/movies.json"
-  )
+  fetch(moviesUrl)
     .then(res => res.json())
-    .then(res =>
+    .then(res => {
       res
         .filter(movie => movie.rating < 3)
         .filter(movie => movie.year >= 2000)
         .map(badMovies => console.log(badMovies.title))
-    )
+    })
     .catch(err => console.log(err))
 }
 fetchBadMovies2000()
