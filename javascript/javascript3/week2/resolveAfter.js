80f9d565bec0a7e14512a7f533b10033f66c27e6
Enqira <moenkira@yahoo.com>
2021-01-08 04:22:35 +0100

    finished promise async functions

--- javascript/javascript3/week2/resolveAfter.js
+++ javascript/javascript3/week2/resolveAfter.js
@@ -1,17 +1,20 @@
 const resolveAfterFunc = resolveAfter => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve("I am called asynchronously")
+      resolve("function run successfully")
     }, resolveAfter * 1000)
   })
 }
 
-// with .then
-resolveAfterFunc(5).then(res => console.log(res))
+resolveAfterFunc(3).then(res => console.log(`Promise ${res}`))
 
 // with asyn await
 async function asyncAwaitFunc(resolveAfter) {
-  const res = await resolveAfterFunc(resolveAfter)
-  console.log(res)
+  try {
+    const res = await resolveAfterFunc(resolveAfter)
+    console.log(`Async ${res}`)
+  } catch (error) {
+    console.log(error)
+  }
 }
-asyncAwaitFunc(3)
+asyncAwaitFunc(5)
