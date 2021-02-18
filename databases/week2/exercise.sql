046c17e6967f1c80858dcd421196f985aea695d1
Enqira <moenkira@yahoo.com>
2021-02-18 23:40:29 +0100

    finished first and second part of HW

---
+++ databases/week2/exercise.sql
@@ -0,0 +1,3 @@
+-- Homework 
+-- Add a task with these attributes: title, description, created, updated, due_date, status_id, user_id
+insert into task (title, description, created, due_date, status_id, user_id) values ('Shoping', 'Buy milk and bread from netto', CURRENT_TIMESTAMP, '2021-02-15 10:00:00', 2, 1);
