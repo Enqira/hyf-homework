ad6227a9cdf0b535ea198a8b35d362922da0f26f
Enqira <moenkira@yahoo.com>
2021-03-01 01:28:58 +0100

    Finished React week1 HW

---
+++ react/week1/todo-app/src/List.js
@@ -0,0 +1,19 @@
+const TodoList = [
+  {
+    id: 1,
+    title: "Get out of bed",
+    deadLine: "Wed Sep 13 2017"
+  },
+  {
+    id: 2,
+    title: "Brush teeth",
+    deadLine: "Thu Sep 14 2017"
+  },
+  {
+    id: 3,
+    title: "Eat breakfast",
+    deadLine: "Fri Sep 15 2017"
+  }
+]
+
+export default TodoList
